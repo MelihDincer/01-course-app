edb5713669dfcbc89a8168140490ff4463ddde67
Melih DİNÇER <m_dincer41@hotmail.com>
2025-05-20 21:40:23 +0300

    20 Mayıs second commit. -Routing continue...

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import HomePage from "./pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
+const routers_1 = (
+  <Route>
+    <Route path="/" element={<HomePage />} />,
+    <Route path="/home" element={<HomePage />} />,
+    <Route path="/about" element={<About />} />,
+    <Route path="/courses" element={<Courses />} />
+  </Route>
+);
+const router_1 = createRoutesFromElements(routers_1);
 
+const routers = [
+  { path: "/", element: <HomePage /> },
+  { path: "/home", element: <HomePage /> },
+  { path: "/about", element: <About /> },
+  { path: "/courses", element: <Courses /> },
+];
+
+const router = createBrowserRouter(router_1);
+
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World!</h1>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+
+export default function HomePage() {
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/courses">Courses</NavLink>
+      </nav>
+      <div>
+        <h1>Home</h1>
+        <p className="lorem">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          commodi, laboriosam sit doloribus nemo quia quo rem quas sunt
+          molestiae repellendus vero, ipsum tempore inventore nam optio
+          exercitationem earum quasi?Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Tenetur harum illum minus fugiat sit modi id
+          repellendus laboriosam blanditiis sequi similique ipsam suscipit
+          provident, corporis nulla praesentium mollitia facere explicabo.
+          Repellat fuga ad ea perferendis, nesciunt, sint corporis quaerat,
+          blanditiis omnis a odio dolore delectus? Aspernatur placeat corporis
+          doloremque delectus vitae aliquid distinctio natus blanditiis, sequi
+          quia consequuntur rerum eaque! Dolore nostrum sed ad facere repellat
+          explicabo beatae similique aperiam non, perspiciatis ab rerum, quidem
+          debitis sunt magni consectetur excepturi illo. At nisi officia aliquid
+          dolor consectetur nulla asperiores porro.
+        </p>
+      </div>
+    </>
+  );
+}
